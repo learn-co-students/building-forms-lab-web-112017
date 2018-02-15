f6ab3a3192204c30fe93abf89e6eb0c3b7dbd126
eal6gf <elum1234@gmail.com>
2018-02-15 18:40:43 -0500

    Done.

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
 
 class BandInput extends Component {
+
+  state = {
+    text: ''
+  }
+  //we have access to store dispatch through 'store' as a prop.
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.props.store.dispatch({
+      type: 'ADD_BAND',
+      band: this.state.text
+    });
+    this.setState({
+      text: ''
+    })
+  }
+
+  handleChange = (event) => {
+    console.log('event target', event.target.value)
+    this.setState({
+      text: event.target.value
+    })
+  }
+
   render() {
     return (
       <div>
-        bands component
+        <form onSubmit={this.handleSubmit}>
+          <input type='text' value={this.state.text} onChange={this.handleChange}></input>
+          <input type='submit'></input>
+        </form>
       </div>
     );
   }
--- src/components/Bands.js
+++ src/components/Bands.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 
 class Bands extends Component {
   render(){
+    const myBands = this.props.store.getState().bands.map((band, index) => {return <li key={index} >{band}</li>})
+    console.log(myBands)
     return (
-      <div>
-        band input
-      </div>
+      <ul>
+      {myBands}
+      </ul>
     );
   }
 };
