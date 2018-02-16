df4fd86791d4049ce32568cbfabc7ca8930a0aa2
ddbrennan <ddbren@gmail.com>
2018-02-16 11:26:36 -0500

    adding bands

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
 
 class BandInput extends Component {
+  state = {
+    text: ""
+  }
+
+  changeInput = (e) => {
+    this.setState({
+      text: e.target.value
+    })
+  }
+
+  submitForm = (e) => {
+    e.preventDefault()
+    this.props.store.dispatch({type: "ADD_BAND", band: this.state.text})
+    this.setState({
+      text: ""
+    })
+  }
+
   render() {
     return (
       <div>
-        bands component
+        <form onSubmit={this.submitForm}>
+          <input type="text" onChange={this.changeInput} value={this.state.text}></input>
+          <input type="submit"></input>
+        </form>
       </div>
     );
   }
