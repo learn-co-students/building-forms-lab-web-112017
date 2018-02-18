8c703a3d040e16b0cd6cc7b9679882c929e88f11
nspp99b <chromepanthers@gmail.com>
2018-02-18 10:13:50 -0500

    built input and display components to modify redux store

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
 
 class BandInput extends Component {
+  
+  state = {
+    text: ""
+  }
+  
+  handleInput = (e) => {
+    this.setState({
+      text: e.target.value
+    })
+  }
+  
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.props.store.dispatch({ 'type': 'ADD_BAND', 'band': this.state.text })
+    this.setState({
+      text: ""
+    })
+  }
+  
   render() {
     return (
       <div>
-        bands component
+        <form onSubmit={(e) => this.handleSubmit(e)}>
+          <input type="text" onChange={(e) => this.handleInput(e)} value={this.state.text}></input>
+        </form>
       </div>
     );
   }
