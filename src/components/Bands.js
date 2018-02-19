309cec488a9b94441f47eec93a3f5adf5adb2908
KellyStamps <kellydsample@gmail.com>
2018-02-19 18:14:46 -0500

    Done.

--- src/components/Bands.js
+++ src/components/Bands.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Bands extends Component {
-  render(){
+  render() {
     return (
       <div>
-        band input
+        <ul>{this.props.store.getState().bands.map(b => <li>{b}</li>)}</ul>
       </div>
     );
   }
-};
+}
 
 export default Bands;
