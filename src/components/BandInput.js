e625cdd49640171d69fafd4c01625f247562a792
Adam Walter <Waltxr@users.noreply.github.com>
2018-06-04 17:11:40 -0400

    Got BandInput component working.

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 
 class BandInput extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      text: ''
+    }
+  }
+
+  handleChange(event) {
+    this.setState({
+      text: event.target.value
+    })
+  }
+
   render() {
     return (
       <div>
-        bands component
+        <form>
+          <label>add band</label>
+          <input
+            type="text"
+            onChange={(event) => this.handleChange(event)} />
+          <input type="submit" />
+        </form>
       </div>
     );
   }
